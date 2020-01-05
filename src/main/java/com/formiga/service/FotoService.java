aa658698796e5a8e73aa7449db033341aaf0a8ad
brunohallef <brunohallef@gmail.com>
2020-01-05 20:51:00 -0300

    Enviando projeto

--- src/main/java/com/formiga/service/FotoService.java
+++ src/main/java/com/formiga/service/FotoService.java
@@ -60,15 +60,6 @@ public class FotoService {
     public File folderResidentPhoto() {
         Path curretPath = Paths.get(".");
         Path absolutPath = curretPath.toAbsolutePath();
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        System.out.println(absolutPath.toString());
-        
         int removePonto = absolutPath.toString().indexOf(".");
         String caminhoFormatado = absolutPath.toString().substring(0, (removePonto - 1));
         File file = new File(caminhoFormatado + "/src/main/resources/static/imagens/resident/");
